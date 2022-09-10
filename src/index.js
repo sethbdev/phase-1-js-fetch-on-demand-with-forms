13c7f930221ed7daf81c2c3f4a23580ce41b8710
Seth <seth.bbrendle@gmail.com>
2022-09-10 11:34:36 -0400

    commit tree c60c5383a1734f1028a1e1509ae8c7cdc933ab51

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 const init = () => {
-  
+    const inputForm = document.querySelector("form");
+
+    inputForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const input = document.querySelector('input#searchByID');
+      
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          const title = document.querySelector("#movieDetails h4")
+          const summary = document.querySelector("#movieDetails p")
+
+          title.textContent = data.title
+          summary.textContent = data.summary
+        });
+
+    })
+
 }
 
 document.addEventListener('DOMContentLoaded', init);
